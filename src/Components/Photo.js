6ae747f1cb93a984cf06491a271854f4473189a5
vinnyvilasboa <vinnycesca@gmail.com>
2021-11-29 16:42:11 -0500

    stateless components

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,12 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Photo extends Component {
-    render() { 
-        const post = this.props.post
-        return <figure className="figure">
-            <img src={post.imageLink} alt={post.description} />
-        </figure>
-    }
+
+function Photo(props){
+    const post = props.post
+    return <figure className="figure">
+        <img className="photo" src={post.imageLink} alt={post.description} />
+        <figcaption> <p> {post.description} </p></figcaption>
+        <div className="button-container">
+        <button className="remove-button">REMOVE</button>
+        </div>
+    </figure>
+
 }
+
+
 
 export default Photo
