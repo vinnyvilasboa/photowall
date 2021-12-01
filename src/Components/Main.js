6618ffda0b0363a7cc43cc7701e601fc368b9ed0
vinnyvilasboa <vinnycesca@gmail.com>
2021-11-30 23:08:29 -0500

    setState for removed post

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,21 +2,7 @@ import React, {Component} from 'react';
 import Title from './Title'
 import PhotoWall from './PhotoWall'
 
-// const posts = [{
-//     id: "0",
-//     description: "beautiful landscape",
-//     imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-//     "3919321_1443393332_n.jpg"
-//     }, {
-//     id: "1",
-//     description: "Aliens???",
-//     imageLink: "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg"
-//     }, {
-//     id: "2",
-//     description: "On a vacation!",
-//     imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
-//     }]
-   
+
 
     
 class Main extends Component {
@@ -39,10 +25,20 @@ class Main extends Component {
                 }]
         }
     }
+
+    removePhoto(postRemoved){
+        console.log(postRemoved.description)
+        this.setState((state) => ({
+            posts: state.posts.filter(post => post !== postRemoved)
+        }))
+
+        //above the idea is to have the state update after onClick to equal to post NOT postRemoved
+
+    }
     render(){
         return   <div>
             <Title title={"PHOTOWALL"}/>
-            <PhotoWall posts= {this.state.posts}/>
+            <PhotoWall posts= {this.state.posts} onRemovePhoto={this.removePhoto}/>
         </div>
     }
 }
