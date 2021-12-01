de28061b0cbc56c15abe5fc6858666b7ec698a9f
vinnyvilasboa <vinnycesca@gmail.com>
2021-11-30 23:36:24 -0500

    updated setState

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -24,6 +24,7 @@ class Main extends Component {
                 imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
                 }]
         }
+        this.removePhoto = this.removePhoto.bind(this)
     }
 
     removePhoto(postRemoved){
