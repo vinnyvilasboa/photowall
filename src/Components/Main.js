ae988a35b62a07bee6061613f2fbfcfa3279988b
vinnyvilasboa <vinnycesca@gmail.com>
2021-12-02 14:49:05 -0500

    issues with element/updates

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,8 +2,10 @@ import React, {Component} from 'react';
 import Title from './Title'
 import PhotoWall from './PhotoWall'
 import AddPhoto from './AddPhoto'
-
-
+import {
+    Routes,
+    Route
+  } from "react-router-dom";
     
 class Main extends Component {
     constructor(){
@@ -57,46 +59,36 @@ class Main extends Component {
     }
 
     render(){
-        // console.log('render')
-        return   <div>
-            {
-                this.state.screen === 'photos' && (
-            <div>
-            <Title title={"PHOTOWALL"}/>
-            <PhotoWall posts= {this.state.posts} onRemovePhoto={this.removePhoto} onNavigate={this.navigate}/>
-            </div>
-                )
-    }
-    {
-        this.state.screen === 'addPhoto' && (
-            <div>
-                <AddPhoto />
-            </div>
-        )
-    }
-        </div>
+        return (
+        <div>
+            <Routes>
+            {/* <Route path="/" render={() => (
+                <div>
+                    <Title title={"PHOTOWALL"} />
+                    <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} onNavigate={this.navigate} />
+                </div> 
+            )}/> */}
+            {/* <Route path="/" element={ <Title title={"PHOTOWALL"} /> }
+            <Route path= "/" element={ <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} onNavigate={this.navigate}/> }/> */}
 
-       
+            {/* <Route path="/" element={ <Title title={"PHOTOWALL"}/> }
+            element={<PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} onNavigate={this.navigate} />} /> */}
+
+         <Route 
+         path="/"  
+         element={<><Title title={"PHOTOWALL"}/>
+                    <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} onNavigate={this.navigate}/></>
+                }
+           
+            <Route path= "/AddPhoto" element = {<AddPhoto/>}/>
+            </Routes>
+
+        </div>
+        )
+
     }
-    
+
 }
 
-//////////////////////SIMULATED FETCH FUNCTION//////////////////////////////////////////
-// function SimulatedFetchFromDB () {
-// return [{
-//     id: "0",
-//     description: "beautiful landscape",
-//     imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-//     "3919321_1443393332_n.jpg"
-//     }, {
-//     id: "1",
-//     description: "Aliens???",
-//     imageLink: "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg"
-//     }, {
-//     id: "2",
-//     description: "On a vacation!",
-//     imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
-//     }]
-// }
 
 export default Main
