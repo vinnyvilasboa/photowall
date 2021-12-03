e4696e80bae0cc2112c8f15a20084ebc42736ae6
vinnyvilasboa <vinnycesca@gmail.com>
2021-12-03 09:04:07 -0500

    app working through new components

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,31 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Title from './Title'
 import PhotoWall from './PhotoWall'
 import AddPhoto from './AddPhoto'
 import {
     Routes,
     Route
-  } from "react-router-dom";
-    
+} from "react-router-dom";
+
 class Main extends Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {
-            posts:[{
+            posts: [{
                 id: "0",
                 description: "beautiful landscape",
                 imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-                "3919321_1443393332_n.jpg"
-                }, {
+                    "3919321_1443393332_n.jpg"
+            }, {
                 id: "1",
                 description: "Aliens???",
                 imageLink: "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg"
-                }, {
+            }, {
                 id: "2",
                 description: "On a vacation!",
                 imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
-                }],
-                screen: 'photos'
+            }],
+            screen: 'photos'
         }
         //empty array bc it's being called below
         this.removePhoto = this.removePhoto.bind(this)
@@ -33,7 +33,7 @@ class Main extends Component {
         // console.log('constructor')
     }
 
-    removePhoto(postRemoved){
+    removePhoto(postRemoved) {
         // console.log(postRemoved.description)
         this.setState((state) => ({
             posts: state.posts.filter(post => post !== postRemoved)
@@ -42,48 +42,39 @@ class Main extends Component {
         //above the idea is to have the state update after onClick to equal to post NOT postRemoved
     }
 
-    navigate(){
+    navigate() {
         this.setState({
             screen: 'addPhoto'
         })
     }
 
-
-    componentDidMount(){
+    componentDidMount() {
         console.log('componentDidMount')
     }
 
-    componentDidUpdate(prevProps, prevState){
+    componentDidUpdate(prevProps, prevState) {
         console.log(prevState.posts)
         console.log(this.state)
     }
-
-    render(){
+/*since react 6 changed a lot i had to update it to match the elements, so far this part has been the most difficult and took me about a day to figure out.. the fragment wrap helped add multiple components to a single path */
+    render() {
         return (
-        <div>
-            <Routes>
-            {/* <Route path="/" render={() => (
-                <div>
-                    <Title title={"PHOTOWALL"} />
-                    <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} onNavigate={this.navigate} />
-                </div> 
-            )}/> */}
-            {/* <Route path="/" element={ <Title title={"PHOTOWALL"} /> }
-            <Route path= "/" element={ <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} onNavigate={this.navigate}/> }/> */}
+            <div>
+                <Routes>
 
-            {/* <Route path="/" element={ <Title title={"PHOTOWALL"}/> }
-            element={<PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} onNavigate={this.navigate} />} /> */}
+                    <Route
+                        path="/"
+                        element={<>
+                            <Title title={"PHOTOWALL"} />
+                            <PhotoWall
+                                posts={this.state.posts}
+                                onRemovePhoto={this.removePhoto}
+                                onNavigate={this.navigate} />
+                        </>} />
 
-         <Route 
-         path="/"  
-         element={<><Title title={"PHOTOWALL"}/>
-                    <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} onNavigate={this.navigate}/></>
-                }
-           
-            <Route path= "/AddPhoto" element = {<AddPhoto/>}/>
-            </Routes>
-
-        </div>
+                    <Route path="/AddPhoto" element={<AddPhoto />} />
+                </Routes>
+            </div>
         )
 
     }
